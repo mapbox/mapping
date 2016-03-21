fcef20170d6fccb481ba99caf8fcee8fe7d231a5
Katy DeCorah <katydecorah@gmail.com>
2016-03-21 14:01:10 -0400

    update test

--- test/mapping.test.js
+++ test/mapping.test.js
@@ -127,6 +127,7 @@ categories.forEach(function(post){
     t.equal(metadata.layout,'category','layout must equal "category"');
     t.ok(metadata.color,'must have a color');
     t.ok(metadata.order,'must have an order');
+    t.equal(typeof metadata.order,'number','order must be a number');
     t.ok(metadata.description,'must have a description');
     
     t.equal(post.indexOf(' '),-1,'file name must not contain spaces');
