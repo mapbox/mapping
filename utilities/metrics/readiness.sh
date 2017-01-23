55bc34110e0ecf6f1ae248359fe1ce3791cc769c
Ramya <ramya@mapbox.com>
2017-01-23 10:15:44 +0530

    cleanup

--- utilities/metrics/readiness.sh
+++ utilities/metrics/readiness.sh
@@ -15,7 +15,7 @@ uploadTo="s3://mapbox/playground/ramyaragupathy/worldcities"
 
 
 #install dependencies
-npm install geojson2poly
+npm install -g geojson2poly
 wget -O - http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -O3 -o osmconvert
 sudo apt-get install jq
 git clone https://github.com/geohacker/osmlazer.git
@@ -47,7 +47,7 @@ do
      wget -c http://download.geofabrik.de/${continents[$i]}/${countries[$i]}/${states[$i]}-latest.osm.pbf
     ./osmconvert --complete-ways  --complex-ways ${states[$i]}-latest.osm.pbf -B=${cities[$i]}.geojson.poly -o=${cities[$i]}.osm.pbf
   else
-  	wget -c http://download.geofabrik.de/${continents[$i]}/${countries[$i]}-latest.osm.pbf
+    wget -c http://download.geofabrik.de/${continents[$i]}/${countries[$i]}-latest.osm.pbf
     ./osmconvert --complete-ways  --complex-ways ${countries[$i]}-latest.osm.pbf -B=${cities[$i]}.geojson.poly -o=${cities[$i]}.osm.pbf
   fi
     echo aws s3 cp ${cities[$i]}.osm.pbf ${uploadTo}/${cities[$i]}/
@@ -56,13 +56,14 @@ do
     node osmlazer/index.js --file ${cities[$i]}.osm.pbf --mode basemap > ${cities[$i]}_basemap.json
     echo aws s3 cp ${cities[$i]}_basemap.json ${uploadTo}/${cities[$i]}/
     aws s3 cp ${cities[$i]}_basemap.json ${uploadTo}/${cities[$i]}/
-    #echo node osmlazer/index.js --file ${cities[$i]}.osm.pbf --mode poi > ${cities[$i]}_poi.json
-    #node osmlazer/index.js --file ${cities[$i]}.osm.pbf --mode poi > ${cities[$i]}_poi.json
-    #echo aws s3 cp ${cities[$i]}_poi.json ${uploadTo}/${cities[$i]}/
-    #aws s3 cp ${cities[$i]}_poi.json ${uploadTo}/${cities[$i]}/
-    # rm ${cities[$i]}.osm.pbf
-    # node osmlazer/index.js --file ${cities[$i]}.osm.pbf --mode address  > ${cities[$i]}_address.json
-    # aws s3 cp ${cities[$i]}_address.json ${uploadTo}/${cities[$i]}/
+    echo node osmlazer/index.js --file ${cities[$i]}.osm.pbf --mode poi > ${cities[$i]}_poi.json
+    node osmlazer/index.js --file ${cities[$i]}.osm.pbf --mode poi > ${cities[$i]}_poi.json
+    echo aws s3 cp ${cities[$i]}_poi.json ${uploadTo}/${cities[$i]}/
+    aws s3 cp ${cities[$i]}_poi.json ${uploadTo}/${cities[$i]}/
+    node osmlazer/index.js --file ${cities[$i]}.osm.pbf --mode address  > ${cities[$i]}_address.json
+    aws s3 cp ${cities[$i]}_address.json ${uploadTo}/${cities[$i]}/
+    rm ${cities[$i]}.osm.pbf
+    
   
 done
 
