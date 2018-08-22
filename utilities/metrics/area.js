61c4fa0293558a4599e7c9ebcb01b4afd869c9c1
Ramya <r.ragupathy85@gmail.com>
2018-08-22 09:36:33 +0530

    indentation (#355)

--- utilities/metrics/area.js
+++ utilities/metrics/area.js
@@ -1,10 +1,7 @@
-var fs = require('fs');
-var turf = require('turf');
-var cities = require('./worldcities.json');
-//console.log(cities);
-console.log('cities'+','+'area');
-cities.features.forEach(function(item){
-	
-
-console.log(item.properties.label+','+turf.area(item)/1000000);
-});
+const fs = require('fs')
+const turf = require('turf')
+const cities = require('./worldcities.json')
+console.log('cities' + ',' + 'area')
+cities.features.forEach(function (item) {
+  console.log(item.properties.label + ',' + turf.area(item) / 1000000)
+})
